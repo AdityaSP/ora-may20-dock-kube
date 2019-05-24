3003a4496235cf082c064ac35512031b88ebc0aa
Aditya <sp.aditya@gmail.com>
2019-05-24 09:30:00 +0530

    Update kube-master.sh

--- k8s/newcluster/kube-master.sh
+++ k8s/newcluster/kube-master.sh
@@ -3,4 +3,4 @@ kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=192
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/canal.yaml
+kubectl apply -f ./canal.yaml
